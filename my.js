d8323f69d8cc1c57a8303ff6b048d262c205b58b
Mikhail-hello <zaswer233@gmail.com>
2025-12-12 11:34:00 +0300

    com10

---
+++ my.js
@@ -0,0 +1,8 @@
+console.info("Hello");
+let x=5**6
+console.log(x, typeof x);
+
+let b=confirm("ты человек?")
+console.log(b)
+//let y=prompt ("Как Вас зовут?", "ВАСИЛИЙ?")
+//alert ("Привет, " + y)
